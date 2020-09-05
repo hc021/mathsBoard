d0ebb513eacefd8eef92e5d8e082c1738c991c23
Choi How <ow21hwoarang@hotmail.com>
2020-09-06 07:59:39 +1000

    add packageJson

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
 import * as app from './modules/vars.js'
 
 
-// const init = function () {
-
-//     const data1 = {
-//         containerId: "question0",
-//         correct: 13,
-//         firstId: "first0",
-//         firstNumber: 78,
-//         id: 0,
-//         secondId: "second0",
-//         secondNumber: 95,
-//         symbol: "-",
-//     }
-//     const data2 = {
-//         containerId: "question0",
-//         correct: 13,
-//         firstId: "first0",
-//         firstNumber: 79,
-//         id: 0,
-//         secondId: "second0",
-//         secondNumber: 95,
-//         symbol: "-",
-//     }
-//     //  const count = app.getParmas();
-//     // const data = app.dataCreator(count);
-//     // console.log(data)
-//     // app.appendQuestion(data1, app.contentParent);
-//     // app.appendQuestion(data2, app.contentParent);
-//     // app.addone(1)
-//     // app.btnSubmit.addEventListener('click', () => {
-//     //     const msgs = app.vaildResult(data);
-//     //     app.displayMsg(msgs);
-//     // })
-// }
-
 document.addEventListener("DOMContentLoaded", app.init)
 
 
